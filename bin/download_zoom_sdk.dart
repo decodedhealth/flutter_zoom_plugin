583ca5ebed834e022b1531c278fa9dfcb74f5a71
Kevin Bayes <kevin@decodedhealth.com>
2020-03-31 18:06:57 +1300

    Update the script

--- bin/download_zoom_sdk.dart
+++ bin/download_zoom_sdk.dart
@@ -3,9 +3,14 @@ import 'dart:io';
 
 
 void main() {
+
+  var location = Platform.script.toString().replaceFirst("file://", "").replaceFirst("/bin/download_zoom_sdk.dart", "");
+
+  print(location);
+
   HttpClient client = new HttpClient();
   var _downloadData = List<int>();
-  var fileSave = new File('./logo.png');
+  var fileSave = new File(location + '/logo.png');
   client.getUrl(Uri.parse("https://assets.website-files.com/5e130ef44b55b08a66410bb1/5e131166946b12bd3abfddd4_logo.png"))
       .then((HttpClientRequest request) {
     return request.close();
